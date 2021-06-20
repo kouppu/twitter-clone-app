7d7d403b7812da076b89082f96d0cfa06e02acdb
kou <suhrr.code@gmail.com>
2021-06-20 23:11:39 +0900

    Refresh mode test

--- server/spec/models/tweet_spec.rb
+++ server/spec/models/tweet_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  describe '#validate' do
-    context '全カラムの値を指定しているとき' do
-      let(:tweet) { build_stubbed(:tweet) }
+  context '全カラムの値を指定しているとき' do
+    let(:tweet) { build_stubbed(:tweet) }
 
-      it '有効である' do
-        expect(tweet).to be_valid
-      end
+    it '有効である' do
+      expect(tweet).to be_valid
     end
+  end
 
-    context 'user_idがnilのとき' do
+  describe '#user_id' do
+    context 'nilのとき' do
       let(:tweet) { build_stubbed(:tweet, user_id: nil) }
       it 'エラーになる' do
         expect(tweet).to_not be_valid
